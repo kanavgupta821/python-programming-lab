164f5c1aef36bb1271f1ce72bed1303bed4ebb2a
kanavgupta821 <43986161+kanavgupta821@users.noreply.github.com>
2018-10-30 14:16:12 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,12 @@
+#Name=Kanav Gupta
+#Division=M
+#Roll no.=25
+import random
+from random import randint as rt
+def game(dice,faces):
+    result=0
+    for roll in range(0,dice):
+        result+=rt(1,faces)
+        return result
+result=game(1,6)
+print(result)
