8649cac49829c67d6eb2f166a1206326d16c5b64
kanavgupta821 <43986161+kanavgupta821@users.noreply.github.com>
2018-10-30 14:12:36 +0530

    Add files via upload

--- rolling dice.py
+++ rolling dice.py
@@ -1,14 +1,12 @@
+#Name=Kanav Gupta
+#Division-M
+#Roll No.-25
 import random
-game_start = input("would you like to roll the dice?")
-def dice_roll():
- print("your number is: " + str(random.randint(1,6)))
- global play_again
- play_again = input("would you like to play again?")
-if game_start == "yes":
- dice_roll()
- while play_again == "yes":
-  dice_roll()
-elif game_start == "no":
- print("game over")
-else:
- print("input not recognised")
+from random import randint as rt
+def game(dice,faces):
+    result=0
+    for roll in range(0,dice):
+        result+=rt(1,faces)
+        return result
+result=game(1,6)
+print(result)
