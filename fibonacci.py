ae2711508710b5ccc2881a7ceed55818a77b6bf4
kanavgupta821 <43986161+kanavgupta821@users.noreply.github.com>
2018-11-27 14:51:20 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,11 @@
+#Fibonnaci series
+#Name-Kanav Gupta
+#Division-M
+#Roll no-25
 n=int(input("enter a number:"))
-a=0
-b=1
-l=[]
+a=0                                   #a is first element of fibonnaci series
+b=1                                   #b is second element of fibonacci series
+l=[]                                  #list is defined
 if n<0:
  print("enter positive")
 elif n==0:
@@ -9,10 +13,10 @@ elif n==0:
 elif n==1:
  print('0,1')
 else:
- l.append(a)
- l.append(b)
+ l.append(a)                          #put the value of a in list
+ l.append(b)                          #put the value of b in list
 for i in range(n-2):
- f=a+b
+ f=a+b                                #next value will always be the sum of previous two values
  l.append(f)
  a=b
  b=f
