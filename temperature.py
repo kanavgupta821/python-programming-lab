fdda4668ce1c677390e3021d472498c5cb6fb7ae
kanavgupta821 <43986161+kanavgupta821@users.noreply.github.com>
2018-11-27 15:06:36 +0530

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,12 @@
+#Tempurature
+#Name-Kanav Gupta
+#Division-M
+#Roll no-25
+x=int(input("Enter temperature:"))                       #input from user
+y=input("is it in 'celcius' or 'farenhite'")
+if y is 'celsius':                                       #if-else statement
+ f=9*x/5 + 32
+ print(f)
+else:
+ c=5/9 *(x-32)
+print(c)
