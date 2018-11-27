46ad9f9a985dfb76349a1da29be4eaa4fa11a1cd
kanavgupta821 <43986161+kanavgupta821@users.noreply.github.com>
2018-11-27 14:45:46 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,11 +1,15 @@
-f=1
+#Factorial 
+#Name-Kanav Gupta
+#Division-M
+#Roll no-25
+f=1                                  #f is initialised to 1
 n=int(input("enter a number:"))
 if n<0:
  print("enter positive")
 elif n==0 or n==1:
  print('1')
 else:
- while n>1:
-  f=f*n
-  n=n-1
+ while n>1:                          #starting of loop
+  f=f*n                              
+  n=n-1                             #the value of n is decreasing by 1
  print(f)
