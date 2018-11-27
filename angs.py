50ed32c78c0f0e49d10873f96b4eba27343dc9e5
kanavgupta821 <43986161+kanavgupta821@users.noreply.github.com>
2018-11-27 14:41:04 +0530

    Update angs.py

--- angs.py
+++ angs.py
@@ -1,13 +1,16 @@
 #define a funtion named angs
+#Name-Kanav Gupta
+#Division-M
+#Roll no-25
 def angs(x):
  s=0                        #s is initialized to 0
  z=x                        #x is a number to be entered  
- while x>0:
-   y=x%10
-   s=y**3+s
-   x=x//10
+ while x>0:                 #starting of while loop
+   y=x%10                   #give remainder
+   s=y**3+s                 #now the cube of remainder is initialised to s
+   x=x//10                  #give quotient
  print(s)
- if s==z:                 
+ if s==z:                  #value of is initialized to z                 
   print("is a angstrom number")
  else:
   print("is not an angstrom number")
