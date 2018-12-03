6d70341ef99924182d9bd2af8ab312e1e5f6a4db
kanavgupta821 <43986161+kanavgupta821@users.noreply.github.com>
2018-12-03 07:39:48 -0800

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -2,11 +2,11 @@
 #Name-Kanav Gupta
 #Division-M
 #Roll no-25
-x=int(input("Enter temperature:"))                       #input from user
-y=input("is it in 'celcius' or 'farenhite'")
-if y is 'celsius':                                       #if-else statement
- f=9*x/5 + 32
+temp=int(input("Enter temperature:"))                       #input from user
+dec=input("if value is in celius say 'y' or else say 'n'")
+if 'y'==dec:                                      #if-else statement
+ f=9*temp/5 +32
  print(f)
 else:
- c=5/9 *(x-32)
+ c=5*temp/9 -32
 print(c)
